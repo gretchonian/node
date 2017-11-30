7fa7a0735c5e4395a32ef2f41916a90fbd79a697
gretchonian <gretchonian@hotmail.com>
2017-11-30 06:33:21 -0800

    clean up

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-var express = require('express')
+//Module dependencies
+var express = require('express') //for HTTP requests/routing
   , app = express()
   , server = app.listen(3000)
-  , bodyParser = require('body-parser')
-  , connection = require('express-myconnection')
+  , bodyParser = require('body-parser') //parse HTML/JSON
+  , connection = require('express-myconnection') //mySQL connection
   , mysql = require('mysql')
   , router = express.Router()
   , expressValidator = require('express-validator')
 
 const { check, validationResult } = require('express-validator/check')
 
+//Configuration
 app.set('views', __dirname + '/views') //set directory to views folder (this is the default)
 app.set('view engine', 'ejs')// set view engine
 app.use(express.Router())
 app.use(expressValidator())
-app.use(function(req,res,next) {
-  console.log(req.method,req.url)
-  next()
-})
 app.use(bodyParser.json()) // parse application/json
 app.use(bodyParser.urlencoded({ extended: true})) //true ==> qs library
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
@@ -58,30 +56,25 @@ usersroute.post(function(req, res, next) {
     res.status(422).json(val_errors)
   } else {
   //prepare output in json format
-  var data = {
-    firstName:req.body.firstName,
-    lastName:req.body.lastName,
-    streetAddress:req.body.streetAddress,
-    city:req.body.city,
-    state:req.body.state,
-    zip:req.body.zip
-  }
-  req.getConnection(function(err, conn){
-    if (err) return next('cannot connect to mysql')
-    var query = conn.query('INSERT INTO users set ?',data, function(err, rows) {
-      if (err) {
-        // console.log(err)
-        return next(err)
-      }
-      res.redirect('back')
+    var data = {
+      firstName:req.body.firstName,
+      lastName:req.body.lastName,
+      streetAddress:req.body.streetAddress,
+      city:req.body.city,
+      state:req.body.state,
+      zip:req.body.zip
+    }
+    req.getConnection(function(err, conn){
+      if (err) return next('cannot connect to mysql')
+      var query = conn.query('INSERT INTO users set ?',data, function(err, rows) {
+        if (err) {
+          return next(err)
+        }
+        res.redirect('back')
+      })
     })
-  })
-}
+  }
 })
-
-// app.get('/main', function(req, res) {
-//   res.render('main')
-// })
 
 app.get('/', function(req, res,) {
   res.send('welcome')   
